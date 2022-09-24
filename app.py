105d61da6a6c986cbf358cbffaee4c1c44db88e3
Anshul <Anshul.pandey7457@gmail.com>
2022-09-25 02:44:56 +0530

    This commit is for WEB APP

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,app,jsonify
 import pickle
 import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -8,22 +9,26 @@ Reg_model=pickle.load(open('model.pkl','rb'))
 
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template("heart.html")
 
 
 @app.route('/predict',methods=['POST','GET'])
 def predict():
-    features=[(float)(x) for x in request.form.values()]
-    final=np.array([features])
-    # print(features)
-    # print(final)
-    prediction=Reg_model.predict(final)
-    if prediction[0] == 1 :
-        return render_template("heart.html",pred='Your Heart is in Danger. \n')
-    else:
-        return render_template("heart.html",pred='Your Heart is safe.\n')
-
+    # features=[(float)(x) for x in request.form.values()]
+    # final=np.array([features])
+    # prediction=Reg_model.predict(final)
+    # if prediction[0] == 1 :
+    #     return render_template("heart.html",pred='Your Heart is in Danger. \n')
+    # else:
+    #     return render_template("heart.html",pred='Your Heart is safe.\n')
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    output=Reg_model.predict(data)
+    print(output[0])
+    return jsonify(output[0])
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
